597e04ef19da8f469ba2357223a88d2fd9ecbc35
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 03:11:25 +0530

    feat: intro() added

--- index.js
+++ index.js
@@ -7,22 +7,18 @@ var qarray1 = [
     ques: "Who was Harry's godfather?\na) Vernon Dursley \nb) Dumbledore \nc) Sirius Black",
     ans: "c",
   }),
-
   (q2 = {
     ques: "Harry's eyes looked like?\n a) James Potter\n b) Lily evans\n c) Sirius Black",
     ans: "b",
   }),
-
   (q3 = {
     ques: "What was the name of harry's bestfriend?\n a) Neville \n b) Draco\n c) Ron",
     ans: "c",
   }),
-
   (q4 = {
     ques: "what is harry's owls name?\n a) Pigwigdeon\n b) Crookshanks\n c) Hedwig",
     ans: "c",
   }),
-
   (q5 = {
     ques: "In which year harry became the captain of the quidditch?\n a) 4rth\n b) 6th\n c) 5th",
     ans: "b",
@@ -34,22 +30,18 @@ var qarray2 = [
     ques: "Who wrote Harry Potter novels?",
     ans: "jk rowling",
   }),
-
   (q2 = {
     ques: "Who gave Harry Potter the lighting bolt mark?",
     ans: "voldemort",
   }),
-
   (q3 = {
     ques: "What are non-magic folks called?",
     ans: "muggle",
   }),
-
   (q4 = {
     ques: "Harry was sorted into which of the four houses?",
     ans: "gryffindor",
   }),
-
   (q5 = {
     ques: "who dropped Harry at his aunts house when he was one?",
     ans: "hagrid",
@@ -83,4 +75,26 @@ function choice() {
   } else {
     console.log("invalid choice");
   }
+}
+function intro() {
+  console.log(chalk.blue.bold("Welcome wizards to the HARRY POTTER QUIZ."));
+  var a = name();
+  while (a === "") {
+    console.log(chalk.red("oh oh! you must enter a name"));
+    var a = name();
+    if (a === "") {
+      continue;
+    } else {
+      break;
+    }
+  }
+
+  console.log(
+    chalk.blue.bold("\nHello " + a + "! These are the rules of the quiz.") +
+      chalk.magenta.italic(
+        "\n1. You get 3 points for each right answer.\n2. You lose 2 points for each wrong answer.\n3. You can only play until your score is above zero.\n4. Only enter a b or c in mcq's."
+      )
+  );
+
+  choice();
 }
