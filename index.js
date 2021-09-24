b6a403eac8e6edf8b28d3f12673f23bccc2b4311
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 03:08:18 +0530

    feat: giving choice to user

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ function name() {
   );
   return userName;
 }
+
+function choice() {
+  console.log(
+    chalk.red.italic.bold(
+      "\nHere you get two choices:\n1. MCQ's \n2. Input type questions"
+    )
+  );
+
+  var choice = readlineSync.question(
+    chalk.blue.bold(
+      "\nEnter your choice in numerals for the questions to reveal!"
+    )
+  );
+
+  if (choice == 1) {
+    qnadisplay(qarray1);
+  } else if (choice == 2) {
+    qnadisplay(qarray2);
+  } else {
+    console.log("invalid choice");
+  }
+}
