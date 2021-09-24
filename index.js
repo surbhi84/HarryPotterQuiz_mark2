e5a6c62d028f87414ad84b40cb4a10b835a972e3
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 03:09:12 +0530

    feat: qarray's added

--- index.js
+++ index.js
@@ -2,6 +2,60 @@ var readlineSync = require("readline-sync");
 var chalk = require("chalk");
 var score = 0;
 
+var qarray1 = [
+  (q1 = {
+    ques: "Who was Harry's godfather?\na) Vernon Dursley \nb) Dumbledore \nc) Sirius Black",
+    ans: "c",
+  }),
+
+  (q2 = {
+    ques: "Harry's eyes looked like?\n a) James Potter\n b) Lily evans\n c) Sirius Black",
+    ans: "b",
+  }),
+
+  (q3 = {
+    ques: "What was the name of harry's bestfriend?\n a) Neville \n b) Draco\n c) Ron",
+    ans: "c",
+  }),
+
+  (q4 = {
+    ques: "what is harry's owls name?\n a) Pigwigdeon\n b) Crookshanks\n c) Hedwig",
+    ans: "c",
+  }),
+
+  (q5 = {
+    ques: "In which year harry became the captain of the quidditch?\n a) 4rth\n b) 6th\n c) 5th",
+    ans: "b",
+  }),
+];
+
+var qarray2 = [
+  (q1 = {
+    ques: "Who wrote Harry Potter novels?",
+    ans: "jk rowling",
+  }),
+
+  (q2 = {
+    ques: "Who gave Harry Potter the lighting bolt mark?",
+    ans: "voldemort",
+  }),
+
+  (q3 = {
+    ques: "What are non-magic folks called?",
+    ans: "muggle",
+  }),
+
+  (q4 = {
+    ques: "Harry was sorted into which of the four houses?",
+    ans: "gryffindor",
+  }),
+
+  (q5 = {
+    ques: "who dropped Harry at his aunts house when he was one?",
+    ans: "hagrid",
+  }),
+];
+
 function name() {
   var userName = readlineSync.question(
     chalk.magenta("TELL me your name..and i'll let you know the rules!")
